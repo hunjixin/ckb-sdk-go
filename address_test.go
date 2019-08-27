6bf5a844453fbaa93d749c80c43a982de7b85a9f
hunjixin <1084400399@qq.com>
2019-08-27 17:08:07 +0800

    add address test

--- address_test.go
+++ address_test.go
@@ -21,9 +21,56 @@ func Test_addressgenerate(t *testing.T) {
 	}
 }
 
-func Test_mainnetAddress(t *testing.T) {
-
+func Test_addressgenerate2(t *testing.T) {
+	priv := EnsurePrivateKey("e79f3207ea4980b7fed79956d5934249ceac4751a4fae01a0f7c4a96884bc4e3")
+	addr := PublicKeyToAddress(priv.PubKey(), testAddresOption)
+	if addr != "ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83" {
+		t.Errorf("expect %s but got %s", "e79f3207ea4980b7fed79956d5934249ceac4751a4fae01a0f7c4a96884bc4e3", addr)
+	}
 }
+
+func Test_GenerateAddress(t *testing.T) {
+	priv :=  EnsurePrivateKey("e79f3207ea4980b7fed79956d5934249ceac4751a4fae01a0f7c4a96884bc4e3")
+	addr := "ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83"
+	option :=  &AddressOptions{
+		prefix:        Testnet,
+		type_:         HashIdx,
+		codeHashIndex: 0x00,
+	}
+	newAddr := PublicKeyToAddress(priv.PubKey(), option)
+	if addr != newAddr {
+		t.Errorf("expect %s but got %s", newAddr, addr)
+	}
+}
+
+func Test_GenerateAddressType(t *testing.T) {
+	priv :=  EnsurePrivateKey("e79f3207ea4980b7fed79956d5934249ceac4751a4fae01a0f7c4a96884bc4e3")
+	addr := "ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83"
+	option :=  &AddressOptions{
+		prefix:        Testnet,
+		type_:         0x01,
+		codeHashIndex: 0x00,
+	}
+	newAddr := PublicKeyToAddress(priv.PubKey(), option)
+	if addr != newAddr {
+		t.Errorf("expect %s but got %s", newAddr, addr)
+	}
+}
+
+func Test_GenerateAddressCodeHash(t *testing.T) {
+	priv :=  EnsurePrivateKey("e79f3207ea4980b7fed79956d5934249ceac4751a4fae01a0f7c4a96884bc4e3")
+	addr := "ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83"
+	option :=  &AddressOptions{
+		prefix:        Testnet,
+		type_:         0x01,
+		codeHashIndex: 0x00,
+	}
+	newAddr := PublicKeyToAddress(priv.PubKey(), option)
+	if addr != newAddr {
+		t.Errorf("expect %s but got %s", newAddr, addr)
+	}
+}
+
 
 func Test_Bech32(t *testing.T) {
 	priv := EnsurePrivateKey("0e79f3207ea4980b7fed79956d5934249ceac4751a4fae01a0f7c4a96884bc4e")
